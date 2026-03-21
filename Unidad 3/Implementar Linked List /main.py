6cdcf842b4325095c85d052d74e8ecbe2d08dc3e
Patricia <glendy.salas99@gmail.com>
2026-03-20 22:45:52 -0600

    Implement basic operations for LinkedList

---
+++ Unidad 3/Implementar Linked List /main.py
@@ -0,0 +1,12 @@
+from lib.MyLinkedList import LinkedList
+
+mi_lista = LinkedList()
+
+mi_lista.insert_at_end(10)
+mi_lista.insert_at_end(20)
+mi_lista.insert_at_start(5)
+
+mi_lista.display()
+
+mi_lista.delete_value(10)
+mi_lista.display()
